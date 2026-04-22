05c9c44eb93bb8f41805b2e93e2f1c1a1403a5ae
Matt <nichols_matt@pm.me>
2026-04-22 11:03:49 -0400

    add qbittorrent kill switch to wg0-up-extra.sh

--- scripts/server/wg0-up-extra.sh
+++ scripts/server/wg0-up-extra.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Pi-hole routing through wg0
 iptables -I FORWARD -i br-pihole -o wg0 -j ACCEPT 2>/dev/null || true
 iptables -I FORWARD -i wg0 -o br-pihole -j ACCEPT 2>/dev/null || true
 iptables -t nat -I POSTROUTING 1 -s 172.20.0.0/24 -o wg0 -j MASQUERADE 2>/dev/null || true
+
+# qBittorrent routing through wg0 only (kill switch)
+iptables -I FORWARD -i br-qbittorrent -o wg0 -j ACCEPT 2>/dev/null || true
+iptables -I FORWARD -i wg0 -o br-qbittorrent -j ACCEPT 2>/dev/null || true
+iptables -t nat -I POSTROUTING 1 -s 172.21.0.0/24 -o wg0 -j MASQUERADE 2>/dev/null || true
+iptables -I FORWARD -i br-qbittorrent -o enp1s0 -j DROP 2>/dev/null || true
+iptables -I FORWARD -i br-qbittorrent -o wlp2s0 -j DROP 2>/dev/null || true
