afe2ef5783781bb1fac5919f2f6940c67f448eed
Matt <nichols_matt@pm.me>
2026-04-22 11:08:05 -0400

    fix qbittorrent subnet to 172.22.0.0/24

--- scripts/server/wg0-up-extra.sh
+++ scripts/server/wg0-up-extra.sh
@@ -8,6 +8,6 @@ iptables -t nat -I POSTROUTING 1 -s 172.20.0.0/24 -o wg0 -j MASQUERADE 2>/dev/nu
 # qBittorrent routing through wg0 only (kill switch)
 iptables -I FORWARD -i br-qbittorrent -o wg0 -j ACCEPT 2>/dev/null || true
 iptables -I FORWARD -i wg0 -o br-qbittorrent -j ACCEPT 2>/dev/null || true
-iptables -t nat -I POSTROUTING 1 -s 172.21.0.0/24 -o wg0 -j MASQUERADE 2>/dev/null || true
+iptables -t nat -I POSTROUTING 1 -s 172.22.0.0/24 -o wg0 -j MASQUERADE 2>/dev/null || true
 iptables -I FORWARD -i br-qbittorrent -o enp1s0 -j DROP 2>/dev/null || true
 iptables -I FORWARD -i br-qbittorrent -o wlp2s0 -j DROP 2>/dev/null || true
